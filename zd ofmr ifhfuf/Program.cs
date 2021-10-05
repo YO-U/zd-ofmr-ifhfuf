8d077e453da28902749ac03a8e66f2524bb961e7
user8 <user8@mail.ru>
2021-10-05 13:22:09 +0200

    Добавьте файлы проекта.

---
+++ zd ofmr ifhfuf/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace zd_ofmr_ifhfuf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите катет");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите гипотенузу");
+            double z = double.Parse(Console.ReadLine());
+            double cat = Math.Sqrt(Math.Pow(x, 2) - Math.Pow(z, 2));
+            double S = (1 / 2) * x * cat;
+            
+            Console.WriteLine("Катет1 ={0:F2}", x);      
+            Console.WriteLine("Катет2={0:F2}", cat); 
+            Console.WriteLine("Гипотенуза ={0:F2}", z);
+            Console.WriteLine("Площадь треугольника ={0:F2}", S);
+        }
+    }
+}
